8dc49b66c343602425aed2f6d9efc3c9bcfefcf4
YesCoach <sbsii1020@gmail.com>
2023-07-20 12:06:03 +0900

    init: Project 생성 및 DatePickerViewController 추가

--- NewlyCoinedWord/DatePickerViewController.swift
+++ NewlyCoinedWord/DatePickerViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DatePickerViewController.swift
 //  NewlyCoinedWord
 //
 //  Created by 박태현 on 2023/07/20.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DatePickerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
 }
-
