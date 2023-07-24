e5585377219d1fd8994065ed40574706c100375a
YesCoach <sbsii1020@gmail.com>
2023-07-25 00:15:57 +0900

    refactor: 버튼 업데이트 로직 개선 - Button 터치와 검색어 입력시 수행했던 로직을 통합 - 신조어 검색 메서드는 항상 수행하므로, 신조어 검색시 버튼 업데이트 하게 구현 - 필요한 옵셔널 바인딩 정리 및 변수 네이밍 수정 - 로직 이해를 위한 주석 정리

--- NewlyCoinedWord/Assignment/NewlyCoinedWord/NewlyCoinedWordViewController.swift
+++ NewlyCoinedWord/Assignment/NewlyCoinedWord/NewlyCoinedWordViewController.swift
@@ -14,7 +14,7 @@ class NewlyCoinedWordViewController: UIViewController {
     @IBOutlet var newWordButton: [UIButton]!
 
     private var data: [String: String] = [:]
-    private var buttonTitle: [String] = []
+    private var currentlyShownData: [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -70,6 +70,7 @@ private extension NewlyCoinedWordViewController {
             $0.contentEdgeInsets = .init(top: 4, left: 8, bottom: 4, right: 8)
         }
     }
+
     func configureData() {
         data = [
             "알잘딱깔센": "알아서 잘 딱 깔끔하게 센스있게",
@@ -83,25 +84,52 @@ private extension NewlyCoinedWordViewController {
     }
 
     func configureButtonData() {
-        let keys = Array(data.keys).shuffled()
+        // data dictionary의 키 값을 배열로 뽑아서, shuffled 수행
+        let newlyCoinedWordList = Array(data.keys).shuffled()
 
+        // newWordButton의 tag를 인덱스로 활용해서 순차적으로 setTitle 수행
+        // 현재 버튼에 표시된 신조어들은 따로 배열로 저장
         newWordButton.forEach {
-            $0.setTitle(keys[$0.tag], for: .normal)
-            buttonTitle.append(keys[$0.tag])
+            let newlyCoinedWord = newlyCoinedWordList[$0.tag]
+            $0.setTitle(newlyCoinedWord, for: .normal)
+            currentlyShownData.append(newlyCoinedWord)
         }
     }
 
-    // 현재 노출된 것 이외의 신조어를 버튼의 타이틀로 설정합니다
-    func changeButtonTitle(button: UIButton) {
-        if let currentTitle = button.currentTitle,
-           let currentIndex = buttonTitle.firstIndex(of: currentTitle),
-           let newTitle = Set(data.keys).subtracting(Set(buttonTitle)).randomElement() {
+    func presentAlert(title: String, message: String, style: UIAlertController.Style) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: style)
+        let alertAction = UIAlertAction(title: "확인", style: .default)
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - 신조어 검색과 버튼 업데이트 로직
+
+private extension NewlyCoinedWordViewController {
+
+    /// 신조어 버튼에 포함된 키워드라면, 해당 버튼의 타이틀을 새로운 신조어로 업데이트 합니다.
+    /// - Parameter keyword: 현재 선택된 신조어 문자열
+    func updateButtonTitle(of keyword: String) {
+        guard let currentIndex = currentlyShownData.firstIndex(of: keyword),
+              let button = newWordButton.filter({ $0.tag == currentIndex }).first
+        else { return }
+
+        // Set의 subtracting을 활용한, 현재 보여지지 않는 신조어 랜덤으로 뽑아오기
+        if let newTitle = Set(data.keys).subtracting(Set(currentlyShownData)).randomElement() {
             button.setTitle(newTitle, for: .normal)
-            buttonTitle.remove(at: currentIndex)
-            buttonTitle.insert(newTitle, at: currentIndex)
+
+            // 새로운 신조어로 대체했으므로, 기존의 신조어는 currentlyShownData에서 제거
+            currentlyShownData.remove(at: currentIndex)
+
+            // 새로운 신조어로 대체했으므로, currentlyShownData에 추가
+            // 단, 버튼의 tag를 인덱스로 써서 배열에 접근하므로 제거한 인덱스에 새로운 신조어 insert하기
+            currentlyShownData.insert(newTitle, at: currentIndex)
         }
     }
 
+    /// 신조어를 검색합니다
+    /// - Parameter keyword: 검색할 신조어 문자열
     func searchNewlyCoinedWord(_ keyword: String) {
         guard !keyword.isEmpty && keyword.count > 1 else {
             presentAlert(title: "", message: "최소 두글자 이상 입력하세요", style: .alert)
@@ -112,21 +140,6 @@ private extension NewlyCoinedWordViewController {
             return
         }
         searchResultLabel.text = result
-
-        if let index = buttonTitle.firstIndex(of: keyword),
-           let button = newWordButton.filter { $0.tag == index }.first {
-               changeButtonTitle(button: button)
-        }
-    }
-
-    func presentAlert(title: String, message: String, style: UIAlertController.Style) {
-        let alert = UIAlertController(
-            title: title,
-            message: message,
-            preferredStyle: style
-        )
-        let alertAction = UIAlertAction(title: "확인", style: .default)
-        alert.addAction(alertAction)
-        present(alert, animated: true)
+        updateButtonTitle(of: keyword)
     }
 }
