59ba70be8476087f4180b4005f4605c0a29c1b63
YesCoach <sbsii1020@gmail.com>
2023-07-25 16:27:11 +0900

    refactor: 신조어 data를 Enum을 활용하도록 리팩토링경 - NewlyCoinedWord Enum 타입의 배열로 변경 - 신조어 뜻은 Enum.description 변수로 사용

--- NewlyCoinedWord/Assignment/NewlyCoinedWord/NewlyCoinedWordViewController.swift
+++ NewlyCoinedWord/Assignment/NewlyCoinedWord/NewlyCoinedWordViewController.swift
@@ -17,8 +17,8 @@ final class NewlyCoinedWordViewController: UIViewController {
 
     // MARK: - Properties
 
-    private var data: [String: String] = [:]
-    private var currentlyShownData: [String] = []
+    private var data: [NewlyCoinedWord] = []
+    private var currentlyShownData: [NewlyCoinedWord] = []
 
     // MARK: - View Life Cycles
 
@@ -83,26 +83,19 @@ private extension NewlyCoinedWordViewController {
     }
 
     func configureData() {
-        data = [
-            "알잘딱깔센": "알아서 잘 딱 깔끔하게 센스있게",
-            "햄최몇": "햄버거 최대 몇개?",
-            "오운완": "오늘 운동 완료",
-            "반모": "반역모의",
-            "세최미": "세계 최고 미드라이너",
-            "사바사": "사람 바이 사람"
-        ]
+        data = NewlyCoinedWord.allCases
         configureButtonData()
     }
 
     func configureButtonData() {
-        // data dictionary의 키 값을 배열로 뽑아서, shuffled 수행
-        let newlyCoinedWordList = Array(data.keys).shuffled()
+        // newlyCoinedWordList의 열거형 배열을 shuffled 진행
+        let newlyCoinedWordList = data.shuffled()
 
         // newWordButton의 tag를 인덱스로 활용해서 순차적으로 setTitle 수행
         // 현재 버튼에 표시된 신조어들은 따로 배열로 저장
         newWordButtonCollection.forEach {
             let newlyCoinedWord = newlyCoinedWordList[$0.tag]
-            $0.setTitle(newlyCoinedWord, for: .normal)
+            $0.setTitle(newlyCoinedWord.rawValue, for: .normal)
             currentlyShownData.append(newlyCoinedWord)
         }
     }
@@ -122,20 +115,21 @@ private extension NewlyCoinedWordViewController {
     /// 신조어 버튼에 포함된 키워드라면, 해당 버튼의 타이틀을 새로운 신조어로 업데이트 합니다.
     /// - Parameter keyword: 현재 선택된 신조어 문자열
     func updateButtonTitle(of keyword: String) {
-        guard let currentIndex = currentlyShownData.firstIndex(of: keyword),
+        guard let newlyCoinedWord = NewlyCoinedWord(rawValue: keyword),
+              let currentIndex = currentlyShownData.firstIndex(of: newlyCoinedWord),
               let button = newWordButtonCollection.filter({ $0.tag == currentIndex }).first
         else { return }
 
         // Set의 subtracting을 활용한, 현재 보여지지 않는 신조어 랜덤으로 뽑아오기
-        if let newTitle = Set(data.keys).subtracting(Set(currentlyShownData)).randomElement() {
-            button.setTitle(newTitle, for: .normal)
+        if let newWordItem = Set(data).subtracting(Set(currentlyShownData)).randomElement() {
+            button.setTitle(newWordItem.rawValue, for: .normal)
 
             // 새로운 신조어로 대체했으므로, 기존의 신조어는 currentlyShownData에서 제거
             currentlyShownData.remove(at: currentIndex)
 
             // 새로운 신조어로 대체했으므로, currentlyShownData에 추가
             // 단, 버튼의 tag를 인덱스로 써서 배열에 접근하므로 제거한 인덱스에 새로운 신조어 insert하기
-            currentlyShownData.insert(newTitle, at: currentIndex)
+            currentlyShownData.insert(newWordItem, at: currentIndex)
         }
     }
 
@@ -146,11 +140,11 @@ private extension NewlyCoinedWordViewController {
             presentAlert(title: "", message: "최소 두글자 이상 입력하세요", style: .alert)
             return
         }
-        guard let result = data[keyword] else {
+        guard let result = NewlyCoinedWord(rawValue: keyword) else {
             presentAlert(title: "", message: "해당하는 신조어에 대한 정보가 없어요", style: .alert)
             return
         }
-        searchResultLabel.text = result
+        searchResultLabel.text = result.description
         updateButtonTitle(of: keyword)
     }
 }
